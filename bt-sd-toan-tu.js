1b1a3b0e3c9bc7b58948797c499e94a7723a91a2
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-05-09 08:56:01 +0700

    bt-js

---
+++ bt-sd-toan-tu.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("10");
+inputHeight = prompt("20");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
